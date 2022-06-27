3e41f63a6e280344b0c66c630f4d8633eb658c55
chen-yijiang <chenyijiang3.14@gmail.com>
2022-06-27 23:01:28 +0800

    update

--- app/src/main/java/com/machinefi/metapebble/pages/activity/AboutActivity.kt
+++ app/src/main/java/com/machinefi/metapebble/pages/activity/AboutActivity.kt
@@ -16,7 +16,7 @@ class AboutActivity: BaseActivity(R.layout.activity_about) {
     override fun initView(savedInstanceState: Bundle?) {
         mTvImei.text = " ${mDevice?.imei}"
         mTvSn.text = "${mDevice?.sn}"
-        mTvVersion.text = "Developer Preview V0.1"
+        mTvVersion.text = "Developer Preview V1.0.0"
 //        mTvAddress.text = "${EncryptUtil.formatAddress(mDevice?.address ?: "").ellipsis(6, 8)}"
 
 //        mIvImeiCopy.setOnClickListener {
